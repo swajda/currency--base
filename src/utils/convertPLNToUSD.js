adfb2897da6187216c002f2e9355c33cd8ac49ec
Szymon Wajda <wasz92@gmail.com>
2022-06-14 11:16:35 +0200

    first commit

---
+++ src/utils/convertPLNToUSD.js
@@ -0,0 +1,20 @@
+export const convertPLNToUSD = (PLN) => {
+
+  if (typeof(PLN) === 'string' || PLN === undefined) {
+    return NaN;
+  }else if (typeof(PLN) !== 'number' && typeof(PLN) !== 'string') {
+    return 'Error'
+  } else if (PLN < 0 ) {
+    PLN = 0
+  }
+
+  const PLNtoUSD = PLN / 3.5;
+
+  const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  });
+
+  return formatter.format(PLNtoUSD).replace(/\u00a0/g, ' ');
+
+}
